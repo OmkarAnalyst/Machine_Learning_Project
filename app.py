6c5da2645c7ba86f3cfc1df6a2875edda7933657
OmkarAnalyst <omkarpanchal22172@gmail.com>
2025-10-06 12:50:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "2cceb687-9fa5-4b0c-8248-e8bb6dc3e489",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
